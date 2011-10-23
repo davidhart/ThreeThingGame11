7cae3219321c09c3787a294ffe641ba7b8f71bf2
Lindsay Cox <_>
2011-10-23 15:51:56 +0000

    Added TTG game solution and Unit class

---
+++ TTG/TTG/TTG/Unit.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace TTG
+{
+    public class Unit
+    {
+        public Rectangle UnitRect;
+        public Texture2D UnitTex;
+        public enum Type
+        {
+            Light,
+            Heavy,
+            Ranged,
+            Aerial,
+            Hero
+        }
+
+        public Type UnitType;
+
+        public void Update()
+        {
+            switch (UnitType)
+            {
+                case Type.Aerial:
+                    {
+                        break;
+                    }
+                case Type.Heavy:
+                    {
+                        break;
+                    }
+                case Type.Hero:
+                    {
+                        break;
+                    }
+                case Type.Light:
+                    {
+                        break;
+                    }
+                case Type.Ranged:
+                    {
+                        break;
+                    }
+            }
+        }
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+        }
+    }
+}
