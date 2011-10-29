60e224b269f67d680401014a13837b1c2aa50b6e
Devon Hansen <_>
2011-10-29 11:59:27 +0000

    System.cs & Particle.cs: Base classes for particle system
    Fire.cs: example on how to use

---
+++ Particles/Fire.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TTG
+{
+    public class Fire : PSystem
+    {
+        public Fire(Game1 game, int numEffects)
+            : base(game, numEffects)
+        {
+        }
+
+        protected override void InitializeConstants()
+        {
+            textureFilename = "fire";
+
+            minInitialSpeed = 0;
+            maxInitialSpeed = 0;
+
+            minAcceleration = 5.0f;
+            maxAcceleration = 7.0f;
+
+            minLifetime = 0.5f;
+            maxLifetime = 4.0f;
+
+            minScale = 0.5f;
+            maxScale = 1.0f;
+
+            minNumParticles = 10;
+            maxNumParticles = 20;
+
+            minRotationSpeed = -MathHelper.PiOver4 / 2.0f;
+            maxRotationSpeed = MathHelper.PiOver4 / 2.0f;
+
+            blendState = BlendState.AlphaBlend;
+
+            DrawOrder = AlphaBlendDrawOrder;
+        }
+    }
+}
